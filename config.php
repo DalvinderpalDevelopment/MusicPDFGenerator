57d54ef7a3d66941b715346facbd178ce4ef2ee1
Dalvinderpal Soora <dalvinderpaldevelopment@gmail.com>
2022-07-30 22:56:29 +0100

    Create config file and config sql settings

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+
+//===================================================================
+
+// CONFIGURATION FILE
+
+//===================================================================
+
+/* Configuration settings for web app are to be placed here */
+
+# SQL Settings
+
+define("sql-username", "root");
+define("sql-password", "password");
+define("sql-database", "musicpdfgenerator-php");
+define("sql-hostname", "localhost");
+define("sql-port", 3306);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
