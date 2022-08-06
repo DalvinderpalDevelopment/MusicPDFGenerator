d102cc08b6ad7d9b9d30169bc76f31eba14eab5a
Dalvinderpal Soora <dalvinderpaldevelopment@gmail.com>
2022-08-06 22:34:39 +0100

    Add image resources and develop index.php and start header development

---
+++ public/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- META TAGS -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Web Application to allow users to access music streaming services and generate PDF files of music lists.">
+        <meta name="author" content="Dalvinderpal Soora">
+        <!-- Open Graph META TAGS -->
+        <meta property="og:title" content="MusicPDFGenerator">
+        <meta propert="og:type" content="Web Application">
+        <meta property="og:url" content="http://localhost/MusicPDFGenerator">
+        <meta propert="og:image" content="assets/images/og-cover.png">
+        <!-- Twitter META TAGS -->
+        <meta property="twitter:title" content="MusicPDFGenerator">
+        <meta property="twitter:type" content="Web Application">
+        <meta property="twitter:url" property="http://localhost/MusicPDFGenerator">
+        <meta property="twitter:image" property="assets/images/og-cover.png">
+        <title>MusicPDFGenerator</title>
+        <!-- Favicon Icon -->
+        <link rel="icon" href="assets/images/favicon.png">
+        <!--Internal CSS Stylesheets -->
+        <link rel="stylesheet" href="asset/css/base.css">
+        <link rel="stylesheet" href="assets/lib/bootstrap/css/bootstrap.min.css">
+        <!-- External Scripts -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <!-- Internal Scripts -->
+        <script src="assets/lib/bootstrap/js/bootstrap.min.js"></script>
+        <script src="assets/js/base.js"></script>
+    </head>
+    <body>
+        <!-- Include Header Section -->
+        <?php include('./views/header.php'); ?>
+        <!-- Include Footer Section -->
+        <?php include('./views/footer.php'); ?>
+    </body>
+</html>
---
+++ public/views/header.php
@@ -0,0 +1,10 @@
+<header>
+    <div class="logo-section container-fluid">
+        <div class="row justify-content-center justify-content-sm-start">
+            <div class="col-4 col-sm-2 p-4">
+                <!-- Insert Logo -->
+                <img src="assets/images/logo-color.svg" alt="MusicPDFGenerator Logo Color Image">
+            </div>
+        </div>
+    </div>
+</header>
